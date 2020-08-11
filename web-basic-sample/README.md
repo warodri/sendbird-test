66fa0e37633549bc83bef55581a7f4153bc2258b
Walter A Rodriguez (UK) <contact@vaskit.com>
2020-08-11 15:04:23 +0100

    First Commit - Walter Test

---
+++ web-basic-sample/README.md
@@ -0,0 +1,23 @@
+# SendBird JavaScript Web Basic Sample
+This is my version for SendBird's Solution Engineer position.
+
+## Run the sample
+1. Install packages
+
+> Require that you have Node v8.x+ installed. 
+
+```bash
+npm install
+```
+
+2. Run
+
+```bash
+npm start
+```
+
+## Changes
+Please find all the screenshots attached to my Email.  
+
+I'm also attaching a POSTMAN script file ready to import and run.
+
---
+++ web-basic-sample/src/js/const.js
@@ -0,0 +1,18 @@
+//export const APP_ID = '9DA1B1F4-0BE6-4DA8-82C5-2E81DAB56F23';
+export const APP_ID = '2E42E26E-9CF0-4BD5-B135-9757930CC2B3';
+export const USER_ID = 'user_id';
+export const DISPLAY_NONE = 'none';
+export const DISPLAY_BLOCK = 'block';
+export const DISPLAY_FLEX = 'flex';
+export const ACTIVE_CLASSNAME = 'active';
+export const KEY_ENTER = 13;
+export const FILE_ID = 'attach_file_id';
+export const UPDATE_INTERVAL_TIME = 5 * 1000;
+export const COLOR_RED = '#DC5960';
+export const MESSAGE_REQ_ID = 'reqId';
+export const OPEN_CHANNEL_SEARCH_URL = 'search_open_channel';
+
+export const body = document.querySelector('body');
+
+// Assessment 3
+export const CUSTOM_TYPE = 'Walter';
